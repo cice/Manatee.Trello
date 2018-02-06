ae10c150bf726b4b19b1a8630257eebe7a5b2b87
Greg Dennis <gregsdennis@yahoo.com>
2018-02-06 17:17:49 +1300

    moved tests around.

---
+++ Manatee.Trello.FunctionalTests/TrelloHost.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using Manatee.Json.Serialization;
+using NUnit.Framework;
+
+namespace Manatee.Trello.FunctionalTests
+{
+	[TestFixture]
+	public class ScriptedTest
+	{
+		[Test]
+		public void Run()
+		{
+			Board board;
+			try
+			{
+				board = Member.Me.Boards.Add($"TestBoard{Guid.NewGuid()}");
+			}
+			finally
+			{
+				board?.Delete();
+			}
+		}
+	}
+}
